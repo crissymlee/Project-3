63521d56df145ca2d52385dacf753f25dc93c583
CoreyGaunt <65140255+CoreyGaunt@users.noreply.github.com>
2020-10-29 12:02:13 -0500

    look at this working code! AW YEAH!

--- static/app.js
+++ static/app.js
@@ -6,15 +6,33 @@ function retrieveInfo(name) {
         var result = metadata.filter(d => d.Artist === name)[0];
         var info = d3.select('#sample-metadata');
         info.html("");
-        Object.entries(result).forEach((d) => {
-            info.append("h6").text(d[0].toUpperCase() + ": " + d[1] + "\n");
+        info.append("h6").text("Artist: " + Object(result.Artist)).attr('font-weight',700);
+        Object.entries(result.Tracks).forEach((d,i) => {
+            info.append("h6").text((i+1) + ": "+ d[1] + "\n");
         });
     });   
 }
 
 function optionChanged(name) {
     retrieveInfo(name);
+    getTracks(name);
 }
+
+function optionChanged2(name) {};
+
+function getTracks(name) {
+    var dropDown2 = d3.select('#selDataset2');
+    d3.json("artist_test2.json").then((data) => {
+        var metadata = data[0].Metadata;
+        var result1 = metadata.filter(d => d.Artist === name)[0];
+        console.log(result1);
+        var tracks = Object.entries(result1.Tracks)
+        dropDown2.html("")
+        tracks.forEach(d => {
+            dropDown2.append("option").text(d[1]).property("value");
+        });
+})
+};
 
 function init() {
     
@@ -25,7 +43,8 @@ function init() {
             dropDown.append("option").text(d).property("value");
         });
         // retrievePlot(name);
-        retrieveInfo(data[0].Artists[0]);
+        retrieveInfo(data[0].Artists[0])
+        getTracks(data[0].Artists[0]);
     });
 };
 
