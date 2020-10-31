f65838495969c520c7eb51ef0e2e584e8b486cdb
CoreyGaunt <65140255+CoreyGaunt@users.noreply.github.com>
2020-10-31 13:59:02 -0500

    pushing my changes to my branch

--- static/js/app.js
+++ static/js/app.js
@@ -19,9 +19,34 @@ function retrieveInfo(name) {
 function optionChanged(name) {
     retrieveInfo(name);
     getDanceability(name);
+    getPopularity(name);
 };
 
 function getDanceability(name) {
+    d3.json("artist_test2.json").then((data) => {
+        var metadata = data[0].Metadata;
+        // console.log(metadata);
+        var artist = metadata.filter(d => d.Artist === name)[0];
+        // console.log(artist);
+        var artist_track_ids = artist.Track_ID;
+        // console.log(artist_track_ids);
+        sp.getAudioFeaturesForTracks(artist_track_ids).then(d => {
+            var results = JSON.stringify(d)
+            // console.log(results);
+            var test = JSON.parse(results);
+            // console.log(test);
+            var track1dan = test.audio_features[0].danceability;
+            var track2dan = test.audio_features[1].danceability;
+            var track3dan = test.audio_features[2].danceability;
+            var track4dan = test.audio_features[3].danceability;
+            var track5dan = test.audio_features[4].danceability;
+            var dance_list = [track1dan,track2dan,track3dan,track4dan,track5dan];
+            // console.log(dance_list);
+            });
+        });   
+    };
+
+function getPopularity(name) {
     d3.json("artist_test2.json").then((data) => {
         var metadata = data[0].Metadata;
         console.log(metadata);
@@ -29,23 +54,45 @@ function getDanceability(name) {
         console.log(artist);
         var artist_track_ids = artist.Track_ID;
         console.log(artist_track_ids);
-        sp.getAudioFeaturesForTracks(artist_track_ids).then(d => {
+        sp.getTracks(artist_track_ids).then(d => {
             var results = JSON.stringify(d)
-            console.log(results);
+            // console.log(results);
             var test = JSON.parse(results);
             console.log(test);
-            var track1dan = test.audio_features[0].danceability;
-            var track2dan = test.audio_features[1].danceability;
-            var track3dan = test.audio_features[2].danceability;
-            var track4dan = test.audio_features[3].danceability;
-            var track5dan = test.audio_features[4].danceability;
-            var dance_list = [track1dan,track2dan,track3dan,track4dan,track5dan];
-            console.log(dance_list);
-            });
-        });   
-    };
+            track1pop = test.tracks[0].popularity;
+            track2pop = test.tracks[1].popularity;
+            track3pop = test.tracks[2].popularity;
+            track4pop = test.tracks[3].popularity;
+            track5pop = test.tracks[4].popularity;
+            tracksPop_list = [track1pop, track2pop, track3pop, track4pop, track5pop];
+            track_names = [test.tracks[0].name, test.tracks[1].name, test.tracks[2].name, test.tracks[3].name, test.tracks[4].name]
+            console.log(tracksPop_list);
 
+            // building bar chart
 
+            var trace = {
+                x: [1,2,3,4,5],
+                y: tracksPop_list,
+                text: track_names,
+                type: "bar",
+                marker: {
+                    color: '#3CB371'
+                }
+            };
+
+            var data = [trace];
+
+            var layout = {
+                title: "Popularity of Selected Artist's Top Five Songs",
+                yaxis: {
+                    tickmode:"linear",
+                    autorange: true
+            }
+        };
+        Plotly.newPlot("bar",data,layout);
+        });
+    });
+};
 
 function init() {
     
@@ -58,6 +105,7 @@ function init() {
         // retrievePlot(name);
         retrieveInfo(data[0].Artists[0]);
         getDanceability(data[0].Artists[0]);
+        getPopularity(data[0].Artists[0]);
     });
 };
 
