869cff5364c1f210adc0f8bd50503997137c2adb
CoreyGaunt <65140255+CoreyGaunt@users.noreply.github.com>
2020-10-31 20:49:52 -0500

    got ALL FIVE songs and they ALL PLAY - LET'S FUCKING GOOOOOOgit add maybe4.htmlgit add maybe4.htmlgit add maybe4.htmlgit add maybe4.html!

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,6 @@
 
 var sp = new SpotifyWebApi();
-sp.setAccessToken('BQA4eaq9q1Z3gBzNK9uIGuhYbb2EO_Cxk5mXGS3z8kJRZu3XUzF9TK9Zk4cGdowdGvc1lZnbzisO6x1TyuI')
+sp.setAccessToken('BQA656Bdu0V1mt3aswYNI6gCrwcbw8i0zeIF1LVrSXT9Nk3kZmEIb190__92lQ2zTD5FJvRNdya_9fFVq18')
 
 function retrieveInfo(name) {
     d3.json("artist_test2.json").then((data) => {
@@ -83,10 +83,84 @@ function getPopularity(name) {
     });
 };
 
+function getEmbed(name) {
+    var embed = d3.select('#play_url')
+    d3.json("artist_test2.json").then((data) => {
+        var metadata = data[0].Metadata;
+        var artist = metadata.filter(d => d.Artist === name)[0];
+        var play_url = artist.Track_Play_URL[0];
+        console.log(play_url);
+        embed
+            .attr("src",play_url)
+            .attr("width","300")
+            .attr("height","100")
+            .attr("frameborder","0")
+            .attr("allowtransparency","true")
+            .attr("allow","encrypted-media");
+    });
+    var embed2 = d3.select('#play_url2')
+    d3.json("artist_test2.json").then((data) => {
+        var metadata = data[0].Metadata;
+        var artist = metadata.filter(d => d.Artist === name)[0];
+        var play_url = artist.Track_Play_URL[1];
+        console.log(play_url);
+        embed2
+            .attr("src",play_url)
+            .attr("width","300")
+            .attr("height","100")
+            .attr("frameborder","0")
+            .attr("allowtransparency","true")
+            .attr("allow","encrypted-media");
+    });
+    var embed3 = d3.select('#play_url3')
+    d3.json("artist_test2.json").then((data) => {
+        var metadata = data[0].Metadata;
+        var artist = metadata.filter(d => d.Artist === name)[0];
+        var play_url = artist.Track_Play_URL[2];
+        console.log(play_url);
+        embed3
+            .attr("src",play_url)
+            .attr("width","300")
+            .attr("height","100")
+            .attr("frameborder","0")
+            .attr("allowtransparency","true")
+            .attr("allow","encrypted-media");
+    });
+    var embed4 = d3.select('#play_url4')
+    d3.json("artist_test2.json").then((data) => {
+        var metadata = data[0].Metadata;
+        var artist = metadata.filter(d => d.Artist === name)[0];
+        var play_url = artist.Track_Play_URL[3];
+        console.log(play_url);
+        embed4
+            .attr("src",play_url)
+            .attr("width","300")
+            .attr("height","100")
+            .attr("frameborder","0")
+            .attr("allowtransparency","true")
+            .attr("allow","encrypted-media");
+    });
+    var embed5 = d3.select('#play_url5')
+    d3.json("artist_test2.json").then((data) => {
+        var metadata = data[0].Metadata;
+        var artist = metadata.filter(d => d.Artist === name)[0];
+        var play_url = artist.Track_Play_URL[4];
+        console.log(play_url);
+        embed5
+            .attr("src",play_url)
+            .attr("width","300")
+            .attr("height","100")
+            .attr("frameborder","0")
+            .attr("allowtransparency","true")
+            .attr("allow","encrypted-media");
+    });
+}
+
 function optionChanged(name) {
     retrieveInfo(name);
     getAudioFeatures(name);
     getPopularity(name);
+    getEmbed(name);
 };
 
 function init() {
@@ -99,6 +173,7 @@ function init() {
         retrieveInfo(data[0].Artists[0]);
         getAudioFeatures(data[0].Artists[0]);
         getPopularity(data[0].Artists[0]);
+        getEmbed(data[0].Artists[0]);
     });
 };
 
